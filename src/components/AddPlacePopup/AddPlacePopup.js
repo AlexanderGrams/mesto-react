6e7d77360537094398f3cb2a427a6727d0f455cb
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-01-23 23:48:09 +0300

    fix: value input in form add card

--- src/components/AddPlacePopup/AddPlacePopup.js
+++ src/components/AddPlacePopup/AddPlacePopup.js
@@ -23,9 +23,9 @@ function AddPlacePopup({isOpen, onClose, onAddCrad}) {
   return (
     <PopupWithForm onSubmit={handleAddPlaceSubmit} name={'add-card'} title={'Новое место'} isOpen={isOpen} onClose={onClose} buttonText="Сохранить">
       <fieldset className="popup__editing-profille">
-        <input id="title-input" className="popup__item popup__item_type_title" type="text" name="name" required minLength="2" maxLength="30" placeholder="Название" onChange={(e)=> {handleChange(e, setNameCard)}} />
+        <input value={nameCard} id="title-input" className="popup__item popup__item_type_title" type="text" name="name" required minLength="2" maxLength="30" placeholder="Название" onChange={(e)=> {handleChange(e, setNameCard)}} />
         <span id="title-input-error" className="popup__text-error"></span>
-        <input id="link-input" className="popup__item popup__item_type_link" type="url" name="link" required placeholder="Ссылка на картинку" onChange={(e)=> {handleChange(e, setLinkCard)}} />
+        <input value={linkCard} id="link-input" className="popup__item popup__item_type_link" type="url" name="link" required placeholder="Ссылка на картинку" onChange={(e)=> {handleChange(e, setLinkCard)}} />
         <span id="link-input-error" className="popup__text-error"></span>
       </fieldset>
     </PopupWithForm>
